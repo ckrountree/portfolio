40df244850a9df5b8ffc8cf4bd8321aa0cf668b6
ckrountree <jmail0207@gmail.com>
2017-08-30 21:36:37 -0700

    move and updated app.js file with cleaner constructor function, added object array in new sourceData.js file

---
+++ js/app.js
@@ -0,0 +1,8 @@
+'use strict';
+
+function Projects ( projDataObj ) {
+  this.title = projDataObj.title;
+  this.languages = projDataObj.languages;  
+  this.date = projDataObj.date;
+  this.company = projDataObj.company;
+}
---
+++ js/sourceData.js
@@ -0,0 +1,23 @@
+'use strict';
+
+var projData = [
+  {
+    title:        'About Me',
+    languages:    'HTML, CSS, Javascript',
+    date:         'May 2017',
+    company:      'Code Fellows, Portland',
+  },
+  {
+    title:        'BusMall',
+    languages:    'HTML, CSS, Javascript',
+    date:         'May 2017',
+    company:      'Code Fellows, Portland',
+  },
+  {
+    title:        'PocketCode',
+    languages:    'HTML, CSS, Javascript',
+    date:         'May 2017',
+    company:      'Code Fellows, Portland',
+  },
+];
+
