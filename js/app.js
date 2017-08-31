ab2f8d47657cd3181f295f2e78687f007b4b5a9c
ckrountree <jmail0207@gmail.com>
2017-08-30 22:16:02 -0700

    start CSS build-out and added comment to js

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 'use strict';
 
+// Create a constructor function of my projects to date.
+
 function Projects ( projDataObj ) {
   this.title = projDataObj.title;
   this.languages = projDataObj.languages;  
