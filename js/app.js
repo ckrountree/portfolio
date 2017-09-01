8abe310a560abc0a34170dc3da2ee8d213c90a1b
ckrountree <jmail0207@gmail.com>
2017-08-31 21:54:55 -0700

    update data attributes

--- js/app.js
+++ js/app.js
@@ -15,12 +15,12 @@ function Projects ( projDataObj ) {
 // attach event listener to nav li links
 $( 'nav a').click( function () {
   $( 'main-nav').hide();
-  var tabs = $( event.target ).attr( 'data-content' );
+  var tabs = $( event.target ).attr( 'data-about' );
   $( 'section#' + tabs ).show();
 
   // create the appearance of a click with a fadeIn
-  // $( 'nav a[data-content="about"]').click();
-  // $( 'nav a[data-content="about"]' ).fadeIn();
-  $( 'nav a[data-content="projects"]').click();
+  $( 'nav a[data-about="about"]').click();
+  // $( 'nav a[data-about="about"]' ).fadeIn();
+  // $( 'nav a[data-projects="projects"]').click();
 
 });
