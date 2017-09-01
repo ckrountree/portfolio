3dca84d4eab11e533e68ce30057df8b7b54e4887
ckrountree <jmail0207@gmail.com>
2017-08-31 21:47:10 -0700

    add icons.css, con't html build-out, add more css, add jquey click functionality

--- js/app.js
+++ js/app.js
@@ -8,3 +8,19 @@ function Projects ( projDataObj ) {
   this.date = projDataObj.date;
   this.company = projDataObj.company;
 }
+
+// hide class = main-nav
+// $( '.main-nav').hide();
+
+// attach event listener to nav li links
+$( 'nav a').click( function () {
+  $( 'main-nav').hide();
+  var tabs = $( event.target ).attr( 'data-content' );
+  $( 'section#' + tabs ).show();
+
+  // create the appearance of a click with a fadeIn
+  // $( 'nav a[data-content="about"]').click();
+  // $( 'nav a[data-content="about"]' ).fadeIn();
+  $( 'nav a[data-content="projects"]').click();
+
+});
